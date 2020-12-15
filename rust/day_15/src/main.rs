e88225272e1e43cd00d5fa25407cc9a7515bfb41
dgoldsb <dgoldsb@live.nl>
2020-12-15 11:11:11 +0100

    Rust day 15 part 1

---
+++ rust/day_15/src/main.rs
@@ -0,0 +1,41 @@
+use aoc::parse_ints;
+use std::collections::HashMap;
+
+fn play_game(inputs: &Vec<isize>, duration: usize) -> (usize, HashMap<usize, usize>) {
+    let mut memory: HashMap<usize, usize> = HashMap::new();
+    let mut last_number: usize = 0;
+
+    for i in 0..duration {
+        let next_number = match inputs.get(i) {
+            Some(m) => *m as usize,
+            None => match memory.get(&last_number) {
+                Some(n) => i - *n,
+                None => 0,
+            },
+        };
+        memory.insert(last_number, i);
+        last_number = next_number;
+    }
+
+    return (last_number, memory);
+}
+
+fn part_a(inputs: &Vec<isize>, duration: usize) -> usize {
+    return play_game(inputs, duration).0;
+}
+
+fn main() {
+    let inputs = parse_ints();
+
+    println!("A: {}", part_a(&inputs, 2020));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part1_example() {
+        assert_eq!(part_a(&vec![0, 3, 6], 2020), 436,);
+    }
+}
